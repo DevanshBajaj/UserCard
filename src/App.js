01fdc61040ef8378f558d007d0bb3a9f1715e2ba
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-23 18:12:59 +0530

    Initial setup and abstract Idea

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserForm from './Components/UserForm';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <section id="user-form">
+        <UserForm/>
+      </section>
+      <section id="user">
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+
+
+    return (
+        <button>Add Users</button>
+    )
+}
+
+export default Button;
